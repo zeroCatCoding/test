b25b9a2355892148e61115ed3b9e269517053e6d
zeroCatCoding <465165188@qq.com>
2020-12-02 23:09:29 +0800

    23.09

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("wojiushiceshigitde 121");
+        System.out.println("wojiushiceshigitde 1211231");
     }
 }
