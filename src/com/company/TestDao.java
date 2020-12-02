7f79fb42d3d0e648020010e87a5a7e3c20730b81
zeroCatCoding <465165188@qq.com>
2020-12-02 22:42:27 +0800

    nicai

---
+++ src/com/company/TestDao.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * 功能描述:
+ *
+ * @Auther: scg
+ * @Date: 2020/12/2 22:38
+ */
+public class TestDao {
+    public static void main(String[] args) {
+        System.out.println("TestDao.main");
+    }
+}
